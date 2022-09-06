5795908f406fa1921900ab8049812f2ef4b93888
Myoungmin <miung222@gmail.com>
2022-09-06 20:48:15 +0900

    Add project files.

---
+++ MFC-WindowsLayout.h
@@ -0,0 +1,32 @@
+﻿
+// MFC-WindowsLayout.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCWindowsLayoutApp:
+// See MFC-WindowsLayout.cpp for the implementation of this class
+//
+
+class CMFCWindowsLayoutApp : public CWinApp
+{
+public:
+	CMFCWindowsLayoutApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCWindowsLayoutApp theApp;
---
+++ MFC-WindowsLayoutDlg.h
@@ -0,0 +1,33 @@
+﻿
+// MFC-WindowsLayoutDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCWindowsLayoutDlg dialog
+class CMFCWindowsLayoutDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCWindowsLayoutDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCWINDOWSLAYOUT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
